95cc8c52ac381b34bf98f0136381c026511ff441
abaena <abaena@ospinternational.com>
2021-01-27 08:45:05 -0500

    correccion error compilacion

--- angular-wishlist-week2/src/app/app.module.ts
+++ angular-wishlist-week2/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { StoreModule as NgRxStoreModule, ActionReducerMap } from '@ngrx/store'
+import { EffectsModule } from '@ngrx/effects'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,12 +11,37 @@ import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
 import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
 import { FormDestinoViajeComponent } from './form-destino-viaje/form-destino-viaje.component';
+import { DestinosApiClient } from './models/destinos-api-client.model';
+import {
+  DestinosViajesState,
+  reducerDestinosViajes,
+  initializeDestinosViajesState,
+  DestinosViajesEffects} from './models/destinos-viajes-state.model';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch : 'full'},
   { path: 'home', component: ListaDestinosComponent},
   { path: 'destino', component: DestinoDetalleComponent}
 ];
+
+// redux init
+
+//estado global de la aplicacion
+export interface AppState {
+  destinos: DestinosViajesState;
+}
+//reducers globales de la aplicacion
+const reducers: ActionReducerMap<AppState> = {
+  destinos: reducerDestinosViajes
+}
+// inicializacion
+let reducersInitialState = {
+  destinos: initializeDestinosViajesState()
+}
+
+// redux fin init
+//
+
 
 @NgModule({
   declarations: [
@@ -28,9 +55,13 @@ const routes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    NgRxStoreModule.forRoot(reducers, {initialState: reducersInitialState }),
+    EffectsModule.forRoot([DestinosViajesEffects])
   ],
-  providers: [],
+  providers: [
+    DestinosApiClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
