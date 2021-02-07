b51b7216b1da3bf17a2d23bc29234c232e32618f
abaena <abaena@ospinternational.com>
2021-02-06 19:55:54 -0500

    rutas anidadas

--- angular-wishlist-week3/src/app/app.module.ts
+++ angular-wishlist-week3/src/app/app.module.ts
@@ -23,6 +23,18 @@ import {
 import { LoginComponent } from './components/login/login/login.component';
 import { ProtectedComponent } from './components/protected/protected/protected.component';
 import { UsuarioLogueadoGuard } from './guards/usuario-logueado/usuario-logueado.guard';
+import { VuelosComponent } from './components/vuelos/vuelos/vuelos.component';
+import { VuelosMainComponent } from './components/vuelos/vuelos-main/vuelos-main.component';
+import { VuelosMasInfoComponent } from './components/vuelos/vuelos-mas-info/vuelos-mas-info.component';
+import { VuelosDetalleComponent } from './components/vuelos/vuelos-detalle/vuelos-detalle.component';
+
+// init routing
+export const childrenRoutesVuelos: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'main', component: VuelosMainComponent },
+  { path: 'mas-info', component: VuelosMasInfoComponent },
+  { path: ':id', component: VuelosDetalleComponent },
+];
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -33,6 +45,12 @@ const routes: Routes = [
     path: 'protected',
     component: ProtectedComponent,
     canActivate: [ UsuarioLogueadoGuard ]
+  },
+  {
+    path: 'vuelos',
+    component: VuelosComponent,
+    canActivate: [ UsuarioLogueadoGuard ],
+    children: childrenRoutesVuelos
   }
 ];
 
@@ -62,7 +80,11 @@ let reducersInitialState = {
     ListaDestinosComponent,
     FormDestinoViajeComponent,
     LoginComponent,
-    ProtectedComponent
+    ProtectedComponent,
+    VuelosComponent,
+    VuelosMainComponent,
+    VuelosMasInfoComponent,
+    VuelosDetalleComponent
   ],
   imports: [
     BrowserModule,
